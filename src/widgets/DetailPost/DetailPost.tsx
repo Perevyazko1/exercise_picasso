59fd9671bddab83381a550824b5b8490b28e2005
Perevyazko <a.perevyazko@gmail.com>
2023-10-07 18:01:04 +0300

    setting  detail page

--- src/widgets/DetailPost/DetailPost.tsx
+++ src/widgets/DetailPost/DetailPost.tsx
@@ -2,6 +2,9 @@ import {memo, ReactNode} from 'react';
 import {classNames, Mods} from "shared/lib/classNames/classNames";
 import {postApi} from "../../shared/api/RtkService";
 import {useParams} from "react-router-dom"
+import {Button, Table} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+import cls from "./DetailPost.module.scss"
 
 
 interface DetailPostProps {
@@ -13,6 +16,7 @@ interface DetailPostProps {
 export const DetailPost = memo((props: DetailPostProps) => {
 
     const { id } = useParams();
+    const navigate = useNavigate()
 
 
 
@@ -30,28 +34,40 @@ export const DetailPost = memo((props: DetailPostProps) => {
 
     return (
         <div
-            className={classNames('', mods, [className])}
+            className={classNames(cls.DetailPost, mods, [className])}
             {...otherProps}
         >
             {isLoading && <h1>Загрузка поста...</h1>}
             {error && <h1>Ошибка загрузки</h1>}
             {data &&
                 <div>
-                    <div>{data.slug}</div>
-                    <div>{data.url}</div>
-                    <div>{data.title}</div>
-                    <div>{data.content}</div>
-                    <img src={data.image}/>
-                    <div>{data.thumbnail}</div>
-                    <div>{data.status}</div>
-                    <div>{data.category}</div>
-                    <div>{data.publishedAt}</div>
-                    <div>{data.updatedAt}</div>
-                    <div>{data.userId}</div>
+                    <p className={cls.Headers}>slug</p>
+                    <div className={cls.Value}>{data.slug}</div>
+                    <p className={cls.Headers}>url</p>
+                    <div className={cls.Value}>{data.url}</div>
+                    <p className={cls.Headers}>title</p>
+                    <div className={cls.Value}>{data.title}</div>
+                    <p className={cls.Headers}>content</p>
+                    <div className={cls.Value}>{data.content}</div>
+                    <p className={cls.Headers}>image</p>
+                    <img className={cls.Value} src={data.image}/>
+                    <p className={cls.Headers}>thumbnail</p>
+                    <div className={cls.Value}>{data.thumbnail}</div>
+                    <p className={cls.Headers}>status</p>
+                    <div className={cls.Value}>{data.status}</div>
+                    <p className={cls.Headers}>category</p>
+                    <div className={cls.Value}>{data.category}</div>
+                    <p className={cls.Headers}>publishedAt</p>
+                    <div className={cls.Value}>{data.publishedAt}</div>
+                    <p className={cls.Headers}>updatedAt</p>
+                    <div className={cls.Value}>{data.updatedAt}</div>
+                    <p className={cls.Headers}>userId</p>
+                    <div className={cls.Value}>{data.userId}</div>
                 </div>
+
             }
 
-
+            <Button className={cls.Button}  variant="secondary" onClick={()=> navigate("/") }>Назад</Button>
 
         </div>
     );
